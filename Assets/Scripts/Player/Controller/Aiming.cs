f9e010c3c18329e93375ed8742ceb34018d7ca22
Alexander Miller <alexmiller183@yahoo.com>
2020-02-16 18:16:30 -0800

    Created simple FPS controller.
    View Scene FPS_Controller_Demo for demonstration. WASD keys for movement, mouse for aiming. Currently no slopes, ground checking, or gravity.

---
+++ Assets/Scripts/Player/Controller/Aiming.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aiming : MonoBehaviour
+{
+    // Represent mouse position, modified by sensitivity.
+    private float mouseX, mouseY;
+
+    // Mouse sensitivity. 100 = 100%.
+    [SerializeField]
+    public float sensitivity = 100f;
+
+    // The transform of the parent player gameObject, NOT THE CAMERA'S.
+    private Transform playerTransform;
+
+    // Value to keep track of up and down camera rotation.
+    float xRotation = 0f;
+
+    private void Awake()
+    {
+        playerTransform = transform.parent;
+    }
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        // Modifying mouse position based on sensitivity and accounting for framerate.
+        mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        // Rotating camera left and right (cannot move parent player gameObject this way, as it will just keep spinning). 
+        transform.Rotate(Vector3.up * mouseX);
+
+        // Setting player rotation equal to camera rotation so that the player gameObject moves and not just the camera.
+        playerTransform.rotation = Quaternion.Euler(0f, transform.eulerAngles.y, 0f);
+
+        // Rotating camera up and down. Making sure that camera's Y axis localRotation is 0 (parent player gameObject should now have the rotation).
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+}
---
+++ Assets/Scripts/Player/Controller/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Represent movement values for horizontal and vertical axes.
+    private float x, z;
+
+    // The CharacterController component of this gameObject.
+    private CharacterController controller;
+
+    // Base speed value.
+    [SerializeField]
+    public float speed = 10f;
+
+    // Speed modifier value. 1 = 100% speed. 0 = 0% speed.
+    [SerializeField]
+    public float modifier = 1;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        // Could normalize move vector to regulate speed at diagonals, but has side effect of wonky deceleration.
+
+        controller.Move(move * speed * modifier * Time.deltaTime);
+    }
+}
