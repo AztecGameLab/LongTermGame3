6bb3ea32482f59518315c06fbccf4375f12b7955
Logan J. Wang <59853630+lwdaolong@users.noreply.github.com>
2020-02-21 01:16:59 -0800

    implemented multiple node spawning
    Only God knows what I have created
    Known Issues:
    - Rooms still overlapping
    - Corner of rooms connect to doors rather than flat wall of room
    - need to implement public parameter for room scale constraints
    - weird deleting logic
    - Don't know if this system is modular enough, might have to go back to the drawing board.

--- Assets/Scripts/ProceduralMapGenerator.cs
+++ Assets/Scripts/ProceduralMapGenerator.cs
@@ -6,24 +6,56 @@ public class ProceduralMapGenerator : MonoBehaviour
 {
     public int maxSize;
     public int numAttempts;
+    public bool isValidPlacementTest;
+    public int startSize;
 
     //A new map is generated on play
     private void Awake()
     {
         Initialize();
     }
+
     //clears the map and generates a new map
     public void Initialize()
     {
-        maxSize = 0;
+        startSize = 0;
+        isValidPlacementTest = false;
+        maxSize = 5;
         if (transform.childCount != 0)
             ClearMap();
-        PlaceRooms(GenerateRoomData());
+        PlaceRooms(GenerateRoomData(startSize, new List<RoomData>(), new RoomData()));
     }
     //edit this function to change the room generation algorithm
-    private List<RoomData> GenerateRoomData()
+    private List<RoomData> GenerateRoomData(int startSize, List<RoomData> rooms, RoomData room)
     {
-        List<RoomData> rooms = new List<RoomData>();
+        if (startSize == 0)
+        {
+            rooms = new List<RoomData>();
+            generateStartRoom(rooms);
+            room = rooms[rooms.Count - 1];
+            startSize++;
+            foreach (DoorData door1 in room.doors)
+            {
+                if (attemptCreateRoom(rooms, door1, room))
+                    GenerateRoomData(startSize+1, rooms, rooms[rooms.Count - 1]); 
+            }
+        }
+        else if (startSize < maxSize)
+        {
+
+
+            //testing attemptRoom DELETE THIS TO SEE DOORS ACTUALLY SPAWN 
+            foreach (DoorData door1 in room.doors)
+            {
+                if (attemptCreateRoom(rooms, door1, room))
+                    GenerateRoomData(startSize + 1, rooms, rooms[rooms.Count - 1]);
+            }
+        }
+        return rooms;
+    }
+
+    private void generateStartRoom(List<RoomData> rooms)
+    {
         RoomData testRoom = new RoomData();
         testRoom.position = Random.insideUnitSphere;
         testRoom.name = "testRoom1";
@@ -35,34 +67,18 @@ public class ProceduralMapGenerator : MonoBehaviour
         int numDoors = Random.Range(1, 5); //finds random door number
         testRoom.doors = new List<DoorData>();
         fillDoors(numDoors, testRoom);
- 
-
-
-        //var secondTestRoom = testRoom;
-        //secondTestRoom.name = "testRoom2";
-        //secondTestRoom.position = Random.insideUnitSphere;
-
 
         rooms.Add(testRoom);
-
-        //testing attemptRoom DELETE THIS TO SEE DOORS ACTUALLY SPAWN 
-        foreach(DoorData door1 in testRoom.doors)
-        {
-            attemptCreateRoom(rooms,door1,testRoom);
-        }
-        //testing attemptRoom
-
-        //rooms.Add(secondTestRoom);
-        return rooms;
     }
+
     //removes all children from this gameobject
     private void ClearMap()
     {
         Transform[] children = transform.GetComponentsInChildren<Transform>();
         foreach (Transform child in children)
         {
-            if(child.gameObject!=gameObject)
-                DestroyImmediate(child.gameObject);
+            if(child.gameObject!=gameObject )
+                Destroy(child.gameObject);
         }
     }
     //creates a new object as a child of this script's transform according to the roomData provided
@@ -143,6 +159,7 @@ public class ProceduralMapGenerator : MonoBehaviour
     {
         for(int i = 0; i< numAttempts; i++)
         {
+
             bool passedAlltests = true;
             RoomData testRoom = new RoomData();
             int width = Random.Range(1, 5);
@@ -152,19 +169,23 @@ public class ProceduralMapGenerator : MonoBehaviour
             testRoom.position = posNearDoor(door, testRoom.size);
             testRoom.name = "testRoom";
             GameObject tempObj2 = tempPlaceRoom(testRoom);
+            tempObj2.GetComponent<BoxCollider>().isTrigger = true;
             foreach (RoomData existRoom in roomList)
             {
                 //kinda jank IDK how to do this efficiently yet
                 GameObject tempObj1 = tempPlaceRoom(existRoom);
 
-                if (!validPlacement(tempObj1,tempObj2))// check if DOES NOT collide with other rooms
+                if (isValidPlacementTest)// check if DOES NOT collide with other rooms
                 {
+                    print("fail");
+                    //!validPlacement(tempObj1,tempObj2)
                     passedAlltests = false;
                 }
-                DestroyImmediate(tempObj1);
+                Destroy(tempObj1);
             }
             if (passedAlltests)
             {
+                testRoom.name = "roomFits";
                 testRoom.type = "Standard";
                 door.room2 = testRoom;
                 int numDoors = Random.Range(2, 4); //finds random door number
@@ -172,10 +193,10 @@ public class ProceduralMapGenerator : MonoBehaviour
                 testRoom.doors.Add(door);
                 fillNewRoomDoors(numDoors, testRoom);
                 roomList.Add(testRoom);
-                DestroyImmediate(tempObj2);
+                Destroy(tempObj2);
                 return true;
             }
-            DestroyImmediate(tempObj2);
+            Destroy(tempObj2);
         }
         
         room.doors.Remove(door);
@@ -230,4 +251,94 @@ public class ProceduralMapGenerator : MonoBehaviour
             return true;
         return false;
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print(collision.gameObject.name);
+        if (collision.gameObject.name.Equals("testRoom"))
+        {
+            isValidPlacementTest = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.gameObject.name);
+        if (other.gameObject.name.Equals("testRoom"))
+        {
+            isValidPlacementTest = true;
+        }
+    }
 }
+
+
+/*
+ * 
+ * 
+ * 
+ * 
+ * 
+ *     private List<RoomData> GenerateRoomData()
+    {
+       // print("GENERATING NEW ROOMS");
+        List<RoomData> rooms = new List<RoomData>();
+        //generateStartRoom(rooms);
+        RoomData testRoom = new RoomData();
+        testRoom.position = Random.insideUnitSphere;
+        testRoom.name = "testRoom1";
+        testRoom.type = "Start";
+        int width = Random.Range(1, 5);
+        int height = Random.Range(1, 5);
+        int length = Random.Range(1, 5);
+        testRoom.size = new Vector3(width, height, length);
+        int numDoors = Random.Range(1, 5); //finds random door number
+        testRoom.doors = new List<DoorData>();
+        fillDoors(numDoors, testRoom);
+
+
+
+        //var secondTestRoom = testRoom;
+        //secondTestRoom.name = "testRoom2";
+        //secondTestRoom.position = Random.insideUnitSphere;
+
+
+        rooms.Add(testRoom);
+        //testing attemptRoom DELETE THIS TO SEE DOORS ACTUALLY SPAWN 
+        foreach (DoorData door1 in testRoom.doors)
+        {
+            attemptCreateRoom(rooms,door1,testRoom);
+        }
+        //testing attemptRoom
+
+        //rooms.Add(secondTestRoom);
+        return rooms;
+    }
+
+
+
+    *      if (startSize == 0)
+        {
+            rooms = new List<RoomData>();
+            generateStartRoom(rooms);
+            room = rooms[rooms.Count - 1];
+            startSize++;
+            foreach (DoorData door1 in room.doors)
+            {
+                if(attemptCreateRoom(rooms, door1, room))
+                    GenerateRoomData(...); increment startSize, use roomData of end of list, 
+            }
+            GenerateRoomData(startSize, rooms);
+        }
+        else if (startSize < maxSize)
+        {
+          
+
+            //testing attemptRoom DELETE THIS TO SEE DOORS ACTUALLY SPAWN 
+            foreach (DoorData door1 in room.doors)
+            {
+                if(attemptCreateRoom(rooms, door1, room))
+                    GenerateRoomData(...);
+            }
+     }
+            return rooms;
+            */
