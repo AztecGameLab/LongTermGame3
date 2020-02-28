a6f7847344f58d4b9bde8e43b591527c8a45b56c
Dan Blackford <dblackford1974@gmail.com>
2020-02-28 10:56:03 -0800

    Added AmmoTypeInfo script

---
+++ Assets/Scripts/AmmoTypeInfo.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoTypeInfo : MonoBehaviour
+{
+    //Temporary list of possible damage types
+    public enum DamageType
+    {
+        Skin,
+        Armor,
+        Phantasmal,
+        Cybernetic,
+        Divine,
+        //...
+        DamageTypeCount
+    }
+
+    //Temporary list of possible projectile effects
+    public enum EffectType
+    {
+        None,
+        Flaming,
+        Exploding,
+        Bouncing,
+        Homing,
+        Randomized,
+    }
+
+    public float baseDamage;
+    public EffectType effectType;
+    public float[] damageTypeFactor;
+
+    public float GetProjectileDamage(WeaponInfo weaponInfo, DamageType damageType)
+    {
+        //return (weaponInfo.baseDamage + baseDamage) * damageTypeFactor[damageType];
+        return 0;
+    }
+
+    public void SetDamageTypeFactor(DamageType damageType, float factor)
+    {
+        damageTypeFactor[(int)damageType] = factor;
+    }
+
+    void Awake()
+    {
+        if (damageTypeFactor == null)
+        {
+            damageTypeFactor = new float[(int)DamageType.DamageTypeCount];
+        }
+
+        for (int i = 0; i < (int)DamageType.DamageTypeCount; i++)
+        {
+            damageTypeFactor[i] = 1.0f;
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
