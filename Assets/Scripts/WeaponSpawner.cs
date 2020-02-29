3640a2a6b92e864ac090e5f47678ca5e14b8ec3e
Jacob Barger <jacob.barger@hotmail.com>
2020-02-28 20:20:12 -0800

    Temporary gun part prefabs

--- Assets/Scripts/WeaponSpawner.cs
+++ Assets/Scripts/WeaponSpawner.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class WeaponSpawner : MonoBehaviour
 {
+    public GameObject newReciever;
+    public GameObject newBarrel;
     private int weaponCount;//keep track of weapons spawned
     [SerializeField]//Can be used to set range of the initial values based on player progress
     private float testProgressModifier = 10.0f;
@@ -25,10 +27,19 @@ public class WeaponSpawner : MonoBehaviour
     //Spawn new weapon, add weapon info component, and set values according to player progress
     void SpawnWeapon()
     {
-        GameObject newWeapon = new GameObject();
         weaponCount++;
+        GameObject newWeapon = new GameObject();
+        //GameObject newWeapon = (GameObject)Instantiate(Resources.Load("TestReciever"));
+        GameObject reciever = Instantiate(newReciever, new Vector3(0, 0, 0), Quaternion.identity);
+        GameObject barrel = Instantiate(newBarrel, reciever.transform.GetChild(0).transform.position, Quaternion.identity);
         newWeapon.AddComponent<WeaponInfo>();
-        newWeapon.GetComponent<WeaponInfo>().weaponName = "Weapon" + weaponCount;
+        newWeapon.name = "Weapon" + weaponCount;
+        newWeapon.GetComponent<WeaponInfo>().weaponName = newWeapon.name;
         newWeapon.GetComponent<WeaponInfo>().SetInitialValues(testProgressModifier);
+        reciever.transform.SetParent(newWeapon.transform);
+        //barrel.transform.SetParent(reciever.transform.GetChild(0).transform);
+        //Instantiate(newBarrel, new Vector3(0, 0, 0), Quaternion.identity);
+        //GameObject barrelSlot = newWeapon.transform.GetChild(0).gameObject;
+        //barrelSlot = newBarrel;
     }
 }
