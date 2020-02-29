19ee6ced90f6deeaa3dea9f7ea6eec8fc556b587
Jacob <jacob@goldenneedham.com>
2020-02-28 21:12:50 -0800

    Updated Driver.cs to have player position

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -2,13 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Driver : MonoBehaviour
+public class Driver : MonoBehaviour
 {
-    public abstract Vector3 GetMovement();
-    public abstract float GetVerticalLook();
-    public abstract float GetHorizontalLook();
-    public abstract bool GetPrimaryWeapon();
-    public abstract bool GetSecondaryWeapon();
-    public abstract bool GetMeleeWeapon();
-    public abstract bool interact();
+    private Transform _player;
+    public Transform player{
+        get{
+            if(_player != null){
+                _player = this.GetComponent<PlayerDriver>().transform;
+            }
+            return _player;
+        }
+    }
+
+    void Awake(){
+        _player = this.GetComponent<PlayerDriver>().transform;
+    }
+
+    public virtual Vector3 GetMovement(){return Vector3.zero;}
+    public virtual float GetVerticalLook(){return 0;}
+    public virtual float GetHorizontalLook(){return 0;}
+    public virtual bool GetPrimaryWeapon(){return false;}
+    public virtual bool GetSecondaryWeapon(){return false;}
+    public virtual bool GetMeleeWeapon(){return false;}
+    public virtual bool interact(){return false;}
 }
