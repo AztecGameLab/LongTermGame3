90c207174b89e162fa0ec895909a5562de6faf04
Dan Blackford <dblackford1974@gmail.com>
2020-03-02 13:27:47 -0800

    Added Projectile Test Scene
    Moving test enemy and wall for projectile testing.  Also initial glitch effect applied periodically to enemy.

---
+++ Assets/TestTarget/TestTarget.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTarget : MonoBehaviour
+{
+    public float range;
+    public float speed;
+
+    Vector3 startPosition;
+ 
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 direction = new Vector3(range, 0, 0);
+
+        float timeOffset = Time.time;
+        transform.position = startPosition + direction * Mathf.Sin(timeOffset * speed);
+    }
+}
