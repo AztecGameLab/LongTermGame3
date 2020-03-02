c4b0fdefd3b95e68094bd4d2e8d2f621ed2899b2
Carter Andrews <andrewscart@gmail.com>
2020-03-01 16:53:43 -0800

    Created ProceduralRoomGenerator. Walls,ceilings, floors, no doors yet

---
+++ Assets/Scripts/ProceduralRoomGenerator.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralRoomGenerator : MonoBehaviour
+{
+    private RoomData myRoom;
+    public bool defaultRoomData=false;
+    public RoomData testData;
+    public GameObject floorTile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (defaultRoomData)
+        {
+            
+            CreateRoom(testData);
+        }
+    }
+    public void CreateRoom(RoomData data)
+    {
+        myRoom = data;
+        transform.position = myRoom.bounds.center;
+        Floor();
+        Walls();
+        Ceiling();
+    }
+    private void Floor()
+    {
+        for(int i = 0; i < myRoom.bounds.size.x;i++)
+        {
+            for (int j = 0; j < myRoom.bounds.size.z;j++)
+            {
+                GameObject tile = Instantiate(floorTile, transform);
+                tile.transform.localPosition = -myRoom.bounds.extents+new Vector3(i, -0.5f, j);
+                tile.transform.rotation = Quaternion.LookRotation(Vector3.down);
+            }
+        }
+    }
+    private void Walls()
+    {
+        for (int i = 0; i < myRoom.bounds.size.y ; i++)
+        {
+            for (int j = 0; j < myRoom.bounds.size.z ; j++)
+            {
+                GameObject tile = Instantiate(floorTile, transform);
+                tile.transform.localPosition = -myRoom.bounds.extents + new Vector3(0.5f, i, j) +Vector3.right*(myRoom.bounds.size.x-1);
+                tile.transform.rotation = Quaternion.LookRotation(Vector3.right);
+                tile = Instantiate(floorTile, transform);
+                tile.transform.localPosition = -myRoom.bounds.extents + new Vector3(-0.5f, i, j) ;
+                tile.transform.rotation = Quaternion.LookRotation(Vector3.left);
+            }
+        }
+        for (int i = 0; i < myRoom.bounds.size.y ; i++)
+        {
+            for (int j = 0; j < myRoom.bounds.size.x ; j++)
+            {
+                GameObject tile = Instantiate(floorTile, transform);
+                tile.transform.localPosition = -myRoom.bounds.extents + new Vector3(j, i, 0.5f) + Vector3.forward * (myRoom.bounds.size.z - 1);
+                tile.transform.rotation = Quaternion.LookRotation(Vector3.forward);
+                tile = Instantiate(floorTile, transform);
+                tile.transform.localPosition = -myRoom.bounds.extents + new Vector3(j, i, -0.5f);
+                tile.transform.rotation = Quaternion.LookRotation(Vector3.back);
+
+            }
+        }
+    }
+    private void Ceiling() {
+        for (int i = 0; i < myRoom.bounds.size.x ; i++)
+        {
+            for (int j = 0; j < myRoom.bounds.size.z ; j++)
+            {
+                GameObject tile = Instantiate(floorTile, transform);
+                tile.transform.localPosition = -myRoom.bounds.extents + new Vector3(i, 0.5f, j)+Vector3.up*(myRoom.bounds.size.y-1);
+                tile.transform.rotation = Quaternion.LookRotation(Vector3.up);
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/RoomData.cs
+++ Assets/Scripts/RoomData.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[System.Serializable]
 public struct RoomData 
 {
     public Bounds bounds;
