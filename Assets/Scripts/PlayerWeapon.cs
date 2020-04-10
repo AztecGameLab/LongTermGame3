2cb70b84e021a993214bd07c63b29a54cf41b726
Dan Blackford <dblackford1974@gmail.com>
2020-04-10 11:46:16 -0700

    Weapon recoil and separate camera for weapon (no clipping through walls)
    Added recoil to the playerweapon in the Projectile Testing scene.  Additional camera now renders player weapon on separate layer to prevent clipping when player is close to the wall.

--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class PlayerWeapon : MonoBehaviour
 {
-    public float targetDistance = 50.0f;                    //Distance to crosshair
-    public float timeToRecenter = 0.5f;
+    public float targetDistance = 50.0f;            //Distance to crosshair
+    public float timeToRecenter = 0.5f;             //Time to recenter shot
+    public float timeToRecover = 0.5f;              //Max recoil recovery time
+    public float recoilTime = 0.1f;
+    public float recoilMaxTilt = 30.0f;
+    public float recoilMaxOffsetZ = 1.0f;
 
     private GameObject weapon;
     private WeaponInfo weaponInfo;
     private ProjectileSpawner projSpawner;
+
+    //For accuracy
     private Vector2 aimOffset;
     private Vector2 fireOffset;
+
+    //For recoil
+    private Vector2 recoilMin;
+    private Vector2 recoilMax;
+    private float recoilSet;   //0 to 1
+    private float recoilCurrent;
+    private float recoilFire;
+
+    //For accuracy / recoil recovery
     private float lastFireTime;
     
     private Quaternion GetAimRotation()
@@ -25,11 +40,42 @@ public class PlayerWeapon : MonoBehaviour
         return Quaternion.LookRotation(target - shootFrom, Vector3.up);
     }
 
+    private void SetRecoil(bool aimSet)
+    {
+        float recoilAngle = Mathf.Lerp(recoilMin.x, recoilMax.x, recoilCurrent);
+        float recoilZ = Mathf.Lerp(recoilMin.y, recoilMax.y, recoilCurrent);
+
+        Vector3 newPos = new Vector3(gameObject.transform.localPosition.x,
+            gameObject.transform.localPosition.y, recoilZ);
+
+        gameObject.transform.localPosition = newPos;
+
+        if (!aimSet) gameObject.transform.rotation = GetAimRotation();
+        Quaternion kickUp = Quaternion.Euler(new Vector3(-recoilAngle, 0, 0));
+        gameObject.transform.rotation *= kickUp;
+    }
+
+    public void SetLayerRecursive(GameObject target, int layer)
+    {
+        Debug.Log($"Setting Layer for {target.gameObject}");
+        target.layer = layer;
+
+        foreach (Transform t in target.transform)
+        {
+            SetLayerRecursive(t.gameObject, layer);
+        }
+    }
+
     private void InitCenterAim()
     {
         lastFireTime = 0;
         fireOffset = Vector2.zero;
         aimOffset = Vector2.zero;
+        recoilMin = new Vector2(0, gameObject.transform.localPosition.z);
+        recoilMax = recoilMin + new Vector2(recoilMaxTilt, -recoilMaxOffsetZ);
+        recoilSet = 0;
+        recoilFire = 0;
+        recoilCurrent = 0;
         gameObject.transform.rotation = GetAimRotation();
     }
 
@@ -37,9 +83,6 @@ public class PlayerWeapon : MonoBehaviour
     {
         if (weapon != null)
         {
-            Debug.Log("Kashingk!");
-            Debug.Log(weaponInfo.accuracy);
-
             float accuracyScale = 5.0f;
             float accuracyFactor = (100.0f - Mathf.Min(weaponInfo.accuracy, 100.0f)) / (100.0f * accuracyScale);
             float scaledErrorX = targetDistance * accuracyFactor;
@@ -47,10 +90,17 @@ public class PlayerWeapon : MonoBehaviour
             aimOffset = aimOffset + new Vector2(Random.Range(-scaledErrorX, scaledErrorX), Random.Range(-scaledErrorY, scaledErrorY));
             
             gameObject.transform.rotation = GetAimRotation();
+            SetRecoil(true);
 
-            fireOffset = aimOffset;
             lastFireTime = Time.time;
             projSpawner.SpawnProjectile();
+
+            fireOffset = aimOffset;
+
+            float recoilFixedTemp = 10.0f;
+            float recoilAmount = Random.Range(0.8f, 1.0f) * (recoilFixedTemp / 100.0f);
+            recoilFire = recoilCurrent;
+            recoilSet = Mathf.Min(recoilCurrent + recoilAmount, 1.0f);
         }
     }
 
@@ -66,7 +116,10 @@ public class PlayerWeapon : MonoBehaviour
         weaponInfo.isEquipped = true;
         InitCenterAim();
 
-        //weaponInfo.isAutomatic = true;
+        gameObject.layer = 13;
+        SetLayerRecursive(weapon, 13);
+
+        weaponInfo.isAutomatic = true;
     }
 
     private void TestCreateWeapon()
@@ -84,10 +137,12 @@ public class PlayerWeapon : MonoBehaviour
     {
         if (weapon == null) TestCreateWeapon();
 
+        bool aimSet = false;
+
+        float delta = Time.time - lastFireTime;
+
         if (aimOffset != Vector2.zero)
         {
-            float delta = Time.time - lastFireTime;
-
             if (delta > timeToRecenter)
             {
                 aimOffset = Vector2.zero;
@@ -96,12 +151,54 @@ public class PlayerWeapon : MonoBehaviour
             {
                 float factor = 1.0f - ((timeToRecenter - delta) / timeToRecenter);
                 factor *= factor;
-                aimOffset.x = Mathf.Lerp(fireOffset.x, 0, factor);
+                aimOffset.x = Mathf.LerpAngle(fireOffset.x, 0, factor);
                 aimOffset.y = Mathf.Lerp(fireOffset.y, 0, factor);
             }
 
             gameObject.transform.rotation = GetAimRotation();
+            aimSet = true;
         }
 
+        if (recoilSet > 0)
+        {
+            //Recoil increasing
+            if (delta >= recoilTime)
+            {
+                recoilCurrent = recoilSet;
+                recoilFire = recoilSet;
+                recoilSet = 0;
+            }
+            else
+            {
+                float f = delta / recoilTime;
+
+                //To do: Switch to quarter sinusoid
+                recoilCurrent = Mathf.Lerp(recoilFire, recoilSet, f);
+            }
+        }
+        else if (recoilCurrent > 0)
+        {
+            //Recoil recovering
+            delta -= recoilTime;
+
+            if (delta >= timeToRecover)
+            {
+                recoilCurrent = 0;
+                recoilFire = 0;
+                recoilSet = 0;
+            }
+            else
+            {
+                float f = delta / timeToRecover;
+
+                //To do: Switch to half sinusoid
+                recoilCurrent = Mathf.Lerp(recoilFire, recoilSet, f);
+            }
+        }
+
+        if (recoilCurrent > 0)
+        {
+            SetRecoil(aimSet);
+        }
     }
 }
