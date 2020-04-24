882570a6a85355d341d19a4e5b787dd5108496c2
Lauryn <lajeff77@gmail.com>
2020-04-24 04:10:09 -0700

    added main menu

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        Debug.Log("Playing game");
+    }
+
+    public void EnterCredits()
+    {
+        Debug.Log("Credits");
+    }
+
+
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+
+
+}
