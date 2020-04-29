4561eda88451ebde96fc90d82e64a07c76545afc
DarkforcesII <darkforcesii@gmail.com>
2020-04-29 15:07:50 -0700

    Added menu music
    Added music for main menu and made sfx and music mixer subgroups

---
+++ Assets/Scripts/Audio/MenuTheme.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuTheme : MonoBehaviour
+{
+    public AudioClip menuClip;
+    private AudioSource musicSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicSource = GetComponent<AudioSource>();
+        StartCoroutine(MenuLoop());
+    }
+
+    IEnumerator MenuLoop()
+    {
+        yield return new WaitForSecondsRealtime(24.548f);
+
+        // make sure the loop volume starts at 0
+        musicSource.volume = 1;
+        musicSource.clip = menuClip;
+        musicSource.Play();
+        musicSource.loop = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
