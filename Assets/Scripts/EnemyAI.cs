3f3fadee646da8ac371adae6e2565cd6d2613276
Kyle McLain Kane <kmclain16@sbcglobal.net>
2020-05-01 17:04:21 -0700

    Enemy02
    Bug fixes and feature additions

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -6,7 +6,6 @@ public class EnemyAI : Driver
 {
     public Transform playerPos;
     public GameObject Enemy02;
-    //public static Rigidbody playerRigidBody;
     UnityEngine.AI.NavMeshAgent myNav;
 
     [SerializeField]
@@ -16,44 +15,30 @@ public class EnemyAI : Driver
     private float currentDifX = 999;
     private float currentDifZ = 999;
     private bool checkOne = false;
-    [SerializeField]
-    private bool inRangeDuringExplosion = false;
-
+    public float killRange = 16f;
     //TODO REMOVE AND PLACE STATIC VARIABLES AFTER TESTING
     [SerializeField]
     private float agroRange = 1500;        //How far until the player agros the enemy
     [SerializeField]
     private float damageToPlayer = 50;   
     [SerializeField]
-    private float timerDuration = 3;        //How long the bomb will blow up
-    int isExploded = 0;
-
-    //Knockback for the player
-    [SerializeField]
-    float radius = 10.0f;
-    [SerializeField]
-    float power = 1000.0f;
-
-    //How many blinks the enemy does per sec
-    public float timerBlink = .2f;
-
-    private bool detonateAnim = false;
+    private float timerDuration = .15f;        //How long the bomb will blow up
+    bool explosion = false;
     bool isVisible = false;
+
+    //Visuals
+    public float timerBlink = .2f;
+    private bool detonateAnim = false;
+    public GameObject Crater;
     public GameObject image;
-    public GameObject player;
-    RaycastHit hit;
-    private GameObject explode1;
-    private GameObject explode2;
+    public GameObject explode1;
+    public GameObject explode2;
     public GameObject explode3;
     public ParticleSystem pe;
-
-    bool explosion = false;
+    
     void Start()
     {
         health = 50;    //Setting the enemy health
-        player = GameObject.Find("Player");
-        explode1 = GameObject.Find("Explode1");
-        explode2 = GameObject.Find("Explode2");
         playerPos = GameObject.FindGameObjectWithTag("Player").transform;
         myNav = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();        
         explode3.SetActive(false);
@@ -77,7 +62,7 @@ public class EnemyAI : Driver
         }
     }
 
-    //Some code used from https://www.reddit.com/r/Unity2D/comments/61e1wn/enemy_line_of_sight_raycast2d_help/
+    
     void FixedUpdate()
     {
         //precompute raysettings
@@ -97,10 +82,15 @@ public class EnemyAI : Driver
             if (sighttest.transform.CompareTag("Player"))
             {
                 isVisible = true;
+                if (!explosion && explosion == false)
+                {
+                    myNav.speed = 7.5f;
+                }
             }
             else
             {
                 isVisible = false;
+                myNav.speed = 0;
             }
         }
 
@@ -112,25 +102,20 @@ public class EnemyAI : Driver
         myNav.SetDestination(playerPos.position);
     }
 
-    private void OnTriggerEnter(Collider other) //If the enemy collides with the player
+    //If the enemy collides with the player
+    private void OnTriggerEnter(Collider other) 
     {
         if(other.tag == "Player" && explosion == false && isVisible == true)
         {
             explosion = true;
-            inRangeDuringExplosion = true;
+            myNav.isStopped = true;
             detonate();
         }       
-    }
-
-    private void OnTriggerExit(Collider other)  //Player leaves the range of the enemy
-    {
-        inRangeDuringExplosion = false;
     }
 
     //Starts the detonation sequence
     void detonate()
     {
-        myNav.speed = 0;
         //Add audio for detonation beeping here 
         StartCoroutine(explosionCounter());     //Freezing the enemy to start the countdown
     }
@@ -140,36 +125,34 @@ public class EnemyAI : Driver
         //Explosion particles and the destruction
         var exp = Enemy02.GetComponent<ParticleSystem>();
         exp.Emit(100);
+
         explode1.SetActive(false);
         explode2.SetActive(false);
-
+        Crater.gameObject.transform.localScale += new Vector3(3, 3, 3);
         //Destroys the image 
-        Destroy(image);             
-        if (inRangeDuringExplosion == true && checkOne == false && isVisible == true)
+        Destroy(image);       
+        
+        if (Distance() < killRange && checkOne == false && isVisible == true)
         {
             checkOne = true;
-
-            //Add explosion audio here
-            
-            knockback();            
+            //Add explosion audio here       
             doDamage();       //Damages the player     
         }
                      
         Destroy(Enemy02, 3);        //Destroys the enemy gameobject after death
     }
 
-    //TODO doesnt work now 
-    //This function is to knock the player back after explosion
-    void knockback()    
+    //Used to calculate distance between the player and the enemy for explosion
+    //purposes
+    private float Distance()
     {
-        //Vector3 explosionPos = transform.position;
-        //playerRigidBody.AddExplosionForce(power, explosionPos, radius, 3.0F);
+        return Vector3.Distance(transform.position, player.transform.position);
     }
 
-    //The Enemy Fucking Dies
+    //The Enemy Dies
     protected override void OnDeath()
     {
-        isExploded++;
+        //Add particle death here to spray out particles
         Destroy(Enemy02);
     }
 
@@ -187,7 +170,7 @@ public class EnemyAI : Driver
         explode3.SetActive(true);
         pe.Play();
 
-        for (int i = 0; i < 20; i++)
+        for (int i = 0; i < 15; i++)
         {
             explode1.SetActive(false);            
             explode2.SetActive(true);
