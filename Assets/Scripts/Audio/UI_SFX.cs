d5f04b02685aef4a645f97071a4c7b7607b9cc68
DarkforcesII <darkforcesii@gmail.com>
2020-05-14 18:16:36 -0700

    Fixed the ui sfx

---
+++ Assets/Scripts/Audio/UI_SFX.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_SFX : MonoBehaviour
+{
+    private AudioSource sfxSource;
+    public AudioClip[] uiSFX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        sfxSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayFirstSFX()
+    {
+        sfxSource.PlayOneShot(uiSFX[0]);
+    }
+
+    public void PlaySecondSFX()
+    {
+        StartCoroutine(SecondSFX());
+    }
+
+    IEnumerator SecondSFX()
+    {
+        sfxSource.PlayOneShot(uiSFX[1]);
+        yield return new WaitForSecondsRealtime(3f);
+        Destroy(gameObject);
+    }
+}
