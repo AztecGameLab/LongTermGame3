a74fd7f6e927c1c0f0dfff73b81c18eb51a143a6
Kain Shepard <kainkun100@gmail.com>
2020-05-30 11:16:54 -0700

    easter egg

---
+++ Assets/Scripts/EasterEgg.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasterEgg : MonoBehaviour
+{
+    int AGL;
+    public GameObject easterEggCanvas;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            AGL = 1;
+        }
+        else if(Input.GetKeyDown(KeyCode.G))
+        {
+            if(AGL == 1)
+                AGL = 2;
+        }
+        else if(Input.GetKeyDown(KeyCode.L))
+        {
+            if(AGL == 2)
+            {
+                easterEggCanvas.SetActive(!easterEggCanvas.activeSelf);
+                AGL = 0;
+            }
+        }
+        else if(Input.anyKeyDown)
+        {
+            AGL = 0;
+        }
+    }
+}
