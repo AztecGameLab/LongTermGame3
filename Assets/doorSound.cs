949e22a38cf66f63f620fcb083214a1aba3b67b8
Kain Shepard <kainkun100@gmail.com>
2020-05-31 16:35:07 -0700

    door sound

---
+++ Assets/doorSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorSound : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    bool played;
+
+    private void Start() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void DoorOpenSound()
+    {
+        if(played)
+            return;
+
+        audioSource.Play();
+        played = true;
+    }
+}
